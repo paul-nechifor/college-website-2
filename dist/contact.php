b18a5cdf72ec928c36b2d32f99965156cc2716c3
Paul Nechifor <paul@nechifor.net>
2017-06-27 22:34:32 +0100

    Update contact info.

--- dist/contact.php
+++ dist/contact.php
@@ -5,8 +5,7 @@ if ($_GET['limba'] == "en")
 	afiseazaAntet('Contact', '', '', 'en');
 	?>
 		<h2>Contact</h2>
-		<p>I can be found only on the internet. I have a phone, but I barely use it (I don't even know my number). If somebody wants to write me something they can do so at <code>irragal at-sign gmail.com</code> or <code>medoxis at-sign yahoo.com</code>.</p>
-		<p>I can also be found on Yahoo Messenger with the ID <code>medoxis</code>.</p>
+		<p>I can be found only on the internet. I have a phone, but I barely use it (I don't even know my number). If somebody wants to write me something they can do so at <code>paul at-sign nechifor.net</code>.</p>
 <?php
 	afiseazaSubsol('en');
 }
@@ -15,8 +14,7 @@ else
 	afiseazaAntet('Contact', '', '', 'ro');	
 	?>
 		<h2>Contact</h2>
-		<p>Pot fi contactat doar pe internet. Am și telefon, dar nu-l prea folosesc (nici nu știu numărul pe de rost). Dacă are cineva ceva să-mi spună poate să-mi scrie la <code>irragal coadăDeMaimuță gmail.com</code> sau <code>medoxis coadăDeMaimuță yahoo.com</code>.</p>
-		<p>Pot fi găsit și pe Yahoo Messenger destul de des cu ID-ul <code>medoxis</code>.</p>
+		<p>Pot fi contactat doar pe internet. Am și telefon, dar nu-l prea folosesc (nici nu știu numărul pe de rost). Dacă are cineva ceva să-mi spună poate să-mi scrie la <code>paul coadăDeMaimuță nechifor.net</code>.</p>
 <?php
 	afiseazaSubsol('ro');
 }	
